26e993676f709d480f9e4596d37f3d5fc8ef69cb
jaime2602 <jaimd86@gamil.com>
2025-02-07 11:31:24 +0100

    entrada salida de datos

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,61 @@ public class Main {
             System.out.println("El numero: "+vi+" es un numero impar");
         }
 
+        //Sentencias de vifurcaciopn anidada
+        if (vi<50){
+            System.out.println("El numero: "+vi+" es un numero < que 50");
+        }else if (vi>50){
+            System.out.println("El numero: "+vi+" es un numero > que 50");
+        }else if (vi==50){
+            System.out.println("El numero: "+vi+" es un numero == que 50");
+        }
+
+        //Sentencias switch case
+        System.out.println("Introduzca color (R/r, V/v, A/a)");
+        cad=sc.next();
+        vc=cad.charAt(0);
+
+        switch (vc){
+            case 'A':
+            case 'a':
+                System.out.println("El color es AZUL ");
+                break;
+            case 'R':
+            case 'r':
+                    System.out.println("El color es ROJO ");
+                break;
+            case 'V':
+            case 'v':
+                        System.out.println("El color es VERDE ");
+                break;
+
+                default:
+                    System.out.println("Color erroneo ");
+        }
+
+
+        if (vc=='A'){
+            System.out.println("OK");
+        }
+
+        //Buclws cuando se conoce el numero de iteraciones
+        int suma1=0, suma2=0;
+        for (int i=0;i<10;i++){
+            suma1=suma1+i;
+            suma2+=i;
+        }
+        System.out.println("SUMA 1: "+suma1 + " SUMA 2: "+suma2);
+        //HACER BUCLE QUE SOLICITE NUMEROS AL USUARIOS HASTA QUE INTRODUZCA UN NUMERO PAR
+        System.out.println("introduce un numero: ");
+        int num= 1;
+        while (num%2!=0 ){
+            num=sc.nextInt();
+            System.out.println("NUMERO par: "+num);
+            if (num%2!=0){
+                System.out.println("El numero: "+num+" es un numero impar");
+            }
+        }
     }
+
 }
+
